64bb490b9191808f8b30409a08115ae2af5cd89d
uchidataku <bbb.rhcp99@gmail.com>
2022-11-03 11:22:26 +0900

    feat: Block構築プログラムの実装

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,13 @@
 fn main() {
-    reduction("4/6");
+    create_block(3);
 }
 
-fn reduction(arg: &str) {
-    let v: Vec<&str> = arg.split('/').collect();
-    let a: u32 = v[0].parse().unwrap();
-    let b: u32 = v[1].parse().unwrap();
-    let greatest_common_divisor: u32 = gcd(a,b);
-    let numerator = 4 / greatest_common_divisor;
-    let denominator = 6 / greatest_common_divisor;
-    println!("{}/{}", numerator, denominator);
-}
-
-fn gcd(num_1: u32, num_2: u32) -> u32 {
-    if num_2 == 0 {
-        num_1
-    } else {
-        gcd(num_2, num_1 % num_2)
+fn create_block(arg: u32) {
+    if arg < 1 {
+        return
+    }
+    for n in 1..=arg {
+        let block = "#";
+        println!("{}", block.repeat(n as usize));
     }
 }
